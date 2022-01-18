a13a62bb70e788873dc8dae56602f6609fc17a4c
gredicer <gredicer@qq.com>
2022-01-18 09:08:31 +0800

    add ChartStudy project

--- VideoStudy/app/src/main/java/com/gredicer/videostudy/MainActivity.kt
+++ VideoStudy/app/src/main/java/com/gredicer/videostudy/MainActivity.kt
@@ -3,9 +3,19 @@ package com.gredicer.videostudy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.VideoView
+
+
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        videoView.setVideoPath("https://cdn.cnbj1.fds.api.mi-img.com/miui-13/connect/connect-mobile-landing_27.mp4")
+        videoView.start()
     }
 }
