3db5f65beffb32e7d3416ebeaef438749f868839
gredicer <gredicer@qq.com>
2022-01-27 10:09:33 +0800

    新建BaseStudy项目

--- ListStudy/app/src/main/java/com/gredicer/liststudy/MainActivity.kt
+++ ListStudy/app/src/main/java/com/gredicer/liststudy/MainActivity.kt
@@ -2,10 +2,14 @@ package com.gredicer.liststudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gredicer.liststudy.base.BaseBindingActivity
+import com.gredicer.liststudy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
+
 }
