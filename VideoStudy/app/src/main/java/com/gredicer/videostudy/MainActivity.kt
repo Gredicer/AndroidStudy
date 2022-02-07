1a09de47f89a2c7fe233e92822f85cd6328f3555
gredicer <gredicer@qq.com>
2022-02-07 15:06:54 +0800

    surfaceview视频播放

--- VideoStudy/app/src/main/java/com/gredicer/videostudy/MainActivity.kt
+++ VideoStudy/app/src/main/java/com/gredicer/videostudy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import com.gredicer.videostudy.base.BaseBindingActivity
 import com.gredicer.videostudy.databinding.ActivityMainBinding
+import com.gredicer.videostudy.ui.SurfaceViewActivity
 import com.gredicer.videostudy.ui.VideoViewActivity
 
 class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
@@ -12,5 +13,6 @@ class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
         super.onCreate(savedInstanceState)
 
         binding.btnVideoView.setOnClickListener { startActivity(Intent(this, VideoViewActivity::class.java)) }
+        binding.btnSurfaceView.setOnClickListener { startActivity(Intent(this, SurfaceViewActivity::class.java)) }
     }
 }
